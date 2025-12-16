86ff07849db6b5edfc2214b235f04c95230d9c62
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-16 07:10:20 +0530

    learn about routing in nextjs

--- jvl-code/my-app/app/about/page.js
+++ jvl-code/my-app/app/about/page.js
@@ -1,7 +1,8 @@
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <h1 className="text-3xl text-red-500 font-bold">Hello</h1>
+      <h1 className="text-3xl text-red-500 font-bold">About Page</h1>
     </main>
   );
 }
+ 
