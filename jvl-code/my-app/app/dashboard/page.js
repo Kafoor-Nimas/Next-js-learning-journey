a5eb030b3128f51625dc08c129d6698076bf4bab
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-16 08:39:43 +0530

    useRouter Hook

--- jvl-code/my-app/app/dashboard/page.js
+++ jvl-code/my-app/app/dashboard/page.js
@@ -1,6 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Dashboard() {
+  const router = useRouter();
+
+  const back = () => {
+    router.push("/");
+  };
   return (
-    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main className="p-8">
+      <button
+        onClick={back}
+        className="bg-blue-500 rounded text-white font-bold py-2 px-4"
+      >
+        Back
+      </button>
       <h1 className="text-3xl text-red-500 font-bold">Dashboard Page</h1>
     </main>
   );
--- jvl-code/my-app/app/dashboard/settings/page.js
+++ jvl-code/my-app/app/dashboard/settings/page.js
@@ -1,6 +1,21 @@
-export default function setting() {
+"use client"
+
+import { useRouter } from "next/navigation";
+
+export default function Setting() {
+  const router = useRouter();
+
+  const back = () => {
+    router.push("/dashboard");
+  };
   return (
-    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main className="p-8">
+      <button
+        onClick={back}
+        className="bg-blue-500 rounded text-white font-bold py-2 px-4"
+      >
+        Back
+      </button>
       <h1 className="text-3xl text-red-500 font-bold">Settings Page</h1>
     </main>
   );
