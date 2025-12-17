e0a7abecfd5f2f38655eb3a622a89897c54f56a7
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-17 17:25:02 +0530

    Navigation Links

--- jvl-code/my-blog/components/Header.js
+++ jvl-code/my-blog/components/Header.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-        <header className="bg-white shadow-lg">
+      <header className="bg-white shadow-lg">
         <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-            <h1 className="text-2xl">Blog</h1>
-            <nav className="space-x-4">
-                <a href="#" className="text-blue-500">Home</a>
-                <a href="#" className="text-blue-500">About</a>
-                <a href="#" className="text-blue-500">Contact</a>
-            </nav>
+          <Link href="/">
+          
+          <h1 className="text-2xl">Blog</h1>
+          </Link>
+          <nav className="space-x-4">
+            <Link href="/" className="text-blue-500">
+              Home
+            </Link>
+            <Link href="/about" className="text-blue-500">
+              About
+            </Link>
+            <Link href="/contact" className="text-blue-500">
+              Contact
+            </Link>
+          </nav>
         </div>
-    </header>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
