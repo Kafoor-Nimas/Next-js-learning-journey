e8c2ef35a60f7d3895ce0f81d3ebe02d0f5d9891
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-23 08:26:52 +0530

    Chnaging date format

--- jvl-code/my-blog/models/postModels.js
+++ jvl-code/my-blog/models/postModels.js
@@ -5,6 +5,7 @@ const postSchema = new Schema(
     title: String,
     description: String,
     image: String,
+    created_at: String,
   },
   { toJSON: { virtuals: true } }
 );
@@ -12,6 +13,28 @@ const postSchema = new Schema(
 postSchema.virtual("shortDescription").get(function () {
   return this.description.substring(0, 50) + "...";
 });
+postSchema.virtual("created_at_formatted").get(function () {
+  return changedateFormate(this.created_at);
+});
+
+function changedateFormate(date_str) {
+  const date = new Date(date_str);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
 
 const PostModel = models.Post || model("Post", postSchema);
 
