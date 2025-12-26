fd266a34f935cceaa5ea9cf35abe4a2c8e34b3df
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-26 08:21:21 +0530

    Adding Validation

--- jvl-code/my-blog/models/enquiryModel.js
+++ jvl-code/my-blog/models/enquiryModel.js
@@ -1,11 +1,20 @@
 import { model, models, Schema } from "mongoose";
 
 const enquirySchema = new Schema({
-  name: String,
-  email: String,
-  message: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
 });
 
-const EnquiryModel = models.Enquiry || model('Enquiry',enquirySchema);
+const EnquiryModel = models.Enquiry || model("Enquiry", enquirySchema);
 
 export default EnquiryModel;
