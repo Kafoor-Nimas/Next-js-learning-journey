8c969a63b0db71d645f47ece25bdd910eeb0efa9
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-12-27 17:44:30 +0530

    Add generateMetadata function to fetch post title for SEO and finish blog post project with jvl code

--- jvl-code/my-blog/app/post/[id]/page.js
+++ jvl-code/my-blog/app/post/[id]/page.js
@@ -1,7 +1,17 @@
 import Post from "../../../components/Post";
 
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+
+  const post = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "/post/" + id
+  ).then((res) => res.json());
+
+  return {
+    title: post.title,
+  };
+}
 
 export default function Page({ params }) {
- 
-  return <Post params={params}/>
+  return <Post params={params} />;
 }
