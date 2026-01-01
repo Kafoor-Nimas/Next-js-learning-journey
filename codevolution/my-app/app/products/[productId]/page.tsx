9288c4f02bd02943d3cfe31ac060dfc5d30bd207
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-01 17:41:42 +0530

    Routing Metadata

--- codevolution/my-app/app/products/[productId]/page.tsx
+++ codevolution/my-app/app/products/[productId]/page.tsx
@@ -1,8 +1,16 @@
-export default async function ProductDetails({
-  params,
-}: {
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  return {
+    title: `Product ${id}`,
+  };
+};
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
   return <h1>Details about product {productId}</h1>;
 }
