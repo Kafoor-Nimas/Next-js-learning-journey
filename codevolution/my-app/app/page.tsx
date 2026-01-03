f611e6eee16b05ed7762212686e1ea71eb4c26b5
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-03 07:13:11 +0530

    link component navigation

--- codevolution/my-app/app/page.tsx
+++ codevolution/my-app/app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcome home!</h1>;
+  return (
+    <>
+      <h1>Welcome home!</h1>
+      <Link href="/blog">
+        <p>Blog</p>
+      </Link>
+      <Link href="/products">
+        <p>Products</p>
+      </Link>
+    </>
+  );
 }
