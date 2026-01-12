9d9a3384466d560185632585e617ad51cd0f5406
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-12 17:27:59 +0530

    Unmatched routes

--- codevolution/my-app/app/complex-dashboard/@notifications/page.tsx
+++ codevolution/my-app/app/complex-dashboard/@notifications/page.tsx
@@ -1,7 +1,13 @@
 import { Card } from "@/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
   return (
-    <Card>Notifications</Card>
-  )
+    <Card>
+      <div>Notifications</div>
+      <div>
+        <Link href="/complex-dashboard/archived">Archived</Link>
+      </div>
+    </Card>
+  );
 }
