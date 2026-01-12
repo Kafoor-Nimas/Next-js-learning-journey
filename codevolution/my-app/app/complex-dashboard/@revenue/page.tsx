a5e7b10bf41c8a3d476dc58a7b7f540e3d96432b
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-12 17:02:25 +0530

    Parallel routes

---
+++ codevolution/my-app/app/complex-dashboard/@revenue/page.tsx
@@ -0,0 +1,6 @@
+
+export default function RevenueMetrics() {
+  return (
+    <div>RevenueMetrics</div>
+  )
+}
---
+++ codevolution/my-app/app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,6 @@
+
+export default function UserAnalytics() {
+  return (
+    <div>UserAnalytics</div>
+  )
+}
