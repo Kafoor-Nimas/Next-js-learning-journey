a1aa395d9d2c0569e0e32263db0be0d8d8e8c066
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-13 18:08:59 +0530

    Middleware and Completed Routing section

---
+++ codevolution/route-handler-demo/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  if (request.nextUrl.pathname === "/profile") {
+    return NextResponse.rewrite(new URL("/hello", request.nextUrl));
+  }
+}
+
+// export const config = {
+//   matcher: "/profile",
+// };
