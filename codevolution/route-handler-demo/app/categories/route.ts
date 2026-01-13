67f35bb328fbc3e7a69da18c1544b089a75d60cd
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-13 17:44:17 +0530

    caching in Route handlers

---
+++ codevolution/route-handler-demo/app/categories/route.ts
@@ -0,0 +1,15 @@
+export const dynamic = "force-static";
+
+export async function GET() {
+  const categories = [
+    {
+      id: 1,
+      name: "Electronics",
+    },
+    {
+      id: 2,
+      name: "Books",
+    },
+  ];
+  return Response.json(categories);
+}
