078958ae7dba1e4c7addc68b06ad06f55b10b36e
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-17 20:00:49 +0530

    Fetching Data in client components

---
+++ codevolution/data-fetching-demo/app/users-client/page.tsx
@@ -0,0 +1,18 @@
+"use client "
+
+import { useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+export default function UserClient() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  return <div>page</div>;
+}
