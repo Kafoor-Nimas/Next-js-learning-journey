8b4f81665a2a72b1867410390c93db1e4f2d84c7
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-01-21 10:26:59 +0530

    react clent and server side component

--- js-mastery/app/page.tsx
+++ js-mastery/app/page.tsx
@@ -1,1 +1,13 @@
-rafce
+import Hello from "./components/Hello";
+
+const Home = () => {
+    console.log("I am a server Component")
+  return (
+    <main>
+      <div>Welcome to Next js</div>
+      <Hello />
+    </main>
+  );
+};
+
+export default Home;
