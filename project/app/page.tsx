7f04a86b14bd6e061f90b2d24591e2eef9e15993
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-02-03 17:01:50 +0530

    created a explore button

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -1,10 +1,15 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const page = () => {
   return (
     <section>
       <h1 className="text-center">
         The Hub for Every Dev <br /> Event You Can't Miss
       </h1>
-      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+      <ExploreBtn />
     </section>
   );
 };
